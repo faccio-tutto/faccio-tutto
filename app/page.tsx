c1595d0e4725421ac7de262e582945ecbe6ab304
faccio-tutto <arch.carmelogallo@gmail.com>
2025-05-15 09:30:15 +0200

    modifica titolo colonna centrale home page

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,9 @@ const MainContent = () => {
                 <div className="w-full md:w-2/2 flex flex-col items-center mt-8 md:mt-0">
                     <section className="relative text-center w-full">
                         <div className="bg-black bg-opacity-50 p-4 md:p-6 flex flex-col items-center">
-                            <h2 className="custom-title">Progettazione - riparazioni - installazioni</h2>
+                            <h2 className="custom-title">PROGETTAZIONE 
+                                <br/>RIPARAZIONI 
+                                <br/>INSTALLAZIONI</h2>
                             <p className="text-yellow-400 text-2xl md:text-2xl font-bold mb-4">Tanti servizi con un solo contatto!</p>
                             <section className="bg-black bg-opacity-80 text-gray-600 rounded-lg shadow-lg p-6 mt-0">
   <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
