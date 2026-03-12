6650da4c195fe4de4657088fd3e3be81bc1e8714
faccio-tutto <arch.carmelogallo@gmail.com>
2026-03-12 19:03:59 +0100

    Immagini galleria fotovoltaico

--- app/preventivoFV/page.tsx
+++ app/preventivoFV/page.tsx
@@ -79,7 +79,8 @@ const PvEstimator: FC = () => {
           { id: "inv8", brand: "Zucchetti Easy power - all in one", modello: "HYD 6000 ZP1", powerKw: 6, price: 1400.00 },
           { id: "inv9", brand: "Zucchetti ZCS Azzurro", modello: "1PH HYD 6000-ZSS HP", powerKw: 6, price: 1300.00 },
           { id: "inv10", brand: "WECO", modello: "5K0 SMART EU All in One", powerKw: 6, price: 1400.00 },
-          { id: "bat11", brand: "CanadianSolar", modello: "EP CUBE", powerKw: 6, price: 1500.00 },
+          { id: "inv11", brand: "CanadianSolar", modello: "EP CUBE", powerKw: 6, price: 1500.00 },
+          { id: "inv12", brand: "Midea", modello: "MHELIOS FLEX-A EA-S6K", powerKw: 6, price: 1100.00 },
         ],
         moduli: [
           { id: "mod1", brand: "Trina", modello: "Vertex S+", powerW: 450, price: 70.00 }, 
@@ -104,6 +105,7 @@ const PvEstimator: FC = () => {
           { id: "bat12", brand: "Zucchetti ZCS Azzurro", modello: "LV ZSX5000 S", capacityKwh: 5.12, price: 950.00 }, 
           { id: "bat13", brand: "WECO", modello: "5K0 SMART HV PRO", capacityKwh: 5.12, price: 1550.00 }, 
           { id: "bat14", brand: "CanadianSolar", modello: "EP CUBE", capacityKwh: 5.12, price: 1300.00 },
+          { id: "bat16", brand: "Midea", modello: "MHELIOS FLEX-A AL10.2-Eo", capacityKwh: 10.24, price: 2720.00 },
         ],
         strutture: [
           { id: "str1", type: "Tetto inclinato (tegole/coppi)", price: 650.00 }, 
